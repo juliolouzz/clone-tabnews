f0ecb03bfddccee7f1070263ccbb95ae4085c14d
Julio Louzano <68581002+juliolouzz@users.noreply.github.com>
2024-02-22 08:48:01 +0000

    modify text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Lorenna, eu amo vc. Se vc me ama da uma risadinha!</h1>
+    return <h1>Lorenna, eu amo vc. Se vc me ama da uma risadinha! ❤️</h1>
 }
 
 export default Home;
