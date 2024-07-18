2d4aefb410ff65c3e23dada168ccb14ae49ff9e1
Julio Louzano <68581002+juliolouzz@users.noreply.github.com>
2024-07-18 08:43:19 +0000

    fix `eslint` linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -3,6 +3,7 @@ const { exec } = require("node:child_process");
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
+  // eslint-disable-next-line no-unused-vars
   function handleReturn(error, stdout, stderr) {
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
